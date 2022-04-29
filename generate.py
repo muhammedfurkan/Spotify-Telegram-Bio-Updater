8b29ea2a5dd68d1c4e8f35898d15100061ce7f1a
M.Furkan <byazade@yandex.com>
2022-04-29 13:33:13 +0300

    code refactor and make a async run generator

--- generate.py
+++ generate.py
@@ -1,15 +1,41 @@
+import asyncio
 import json
 
-import requests
+from aiohttp import ClientSession
 
 from constants import CLIENT_ID, CLIENT_SECRET, INITIAL_BIO, INITIAL_TOKEN
 
-body = {"client_id": CLIENT_ID, "client_secret": CLIENT_SECRET,
-        "grant_type": "authorization_code", "redirect_uri": "https://example.com/callback",
-        "code": INITIAL_TOKEN}
-r = requests.post("https://accounts.spotify.com/api/token", data=body)
-save = r.json()
-to_create = {'bio': INITIAL_BIO, 'access_token': save['access_token'], 'refresh_token': save['refresh_token'],
-             'telegram_spam': False, 'spotify_spam': False}
-with open('./database.json', 'w') as outfile:
-    json.dump(to_create, outfile, indent=4, sort_keys=True)
+body = {
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
+    "grant_type": "authorization_code",
+    "redirect_uri": "http://localhost:1234/callback",
+    "code": INITIAL_TOKEN,
+}
+
+
+async def generate():
+    async with ClientSession() as session:
+        async with session.post(
+            "https://accounts.spotify.com/api/token", data=body
+        ) as post_response:
+            save = await post_response.json()
+            to_create = {
+                "bio": INITIAL_BIO,
+                "access_token": save["access_token"],
+                "refresh_token": save["refresh_token"],
+                "telegram_spam": False,
+                "spotify_spam": False,
+            }
+            with open("./database.json", "w") as outfile:
+                json.dump(to_create, outfile, indent=4, sort_keys=True)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(generate())
+    loop.close()
+
+
+if __name__ == "__main__":
+    main()
